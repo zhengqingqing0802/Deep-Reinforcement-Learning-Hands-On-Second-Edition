236d4ba01456528e39270b2660f63e49ff6b97c9
simondlevy <simon.d.levy@gmail.com>
2020-06-30 21:30:18 -0400

    More command-line migration work

--- Chapter20/04_contnious_ga.py
+++ Chapter20/04_contnious_ga.py
@@ -13,8 +13,6 @@ from lib import make_parser_with_seed
 
 from tensorboardX import SummaryWriter
 
-NOISE_STD = 0.01
-
 class Net(nn.Module):
     def __init__(self, obs_size, act_size, hid_size):
         super(Net, self).__init__()
@@ -50,22 +48,22 @@ def evaluate(args):
     return reward, steps
 
 
-def mutate_net(net, seed, copy_net=True):
+def mutate_net(net, seed, noise_std, copy_net=True):
     new_net = copy.deepcopy(net) if copy_net else net
     np.random.seed(seed)
     for p in new_net.parameters():
         noise = np.random.normal(size=p.data.size())
         noise_t = torch.FloatTensor(noise)
-        p.data += NOISE_STD * noise_t
+        p.data += noise_std * noise_t
     return new_net
 
 
-def build_net(env, seeds, nhid):
+def build_net(env, seeds, nhid, noise_std):
     torch.manual_seed(seeds[0])
     net = Net(env.observation_space.shape[0],
               env.action_space.shape[0], nhid)
     for seed in seeds[1:]:
-        net = mutate_net(net, seed, copy_net=False)
+        net = mutate_net(net, seed, noise_std, copy_net=False)
     return net
 
 
@@ -73,7 +71,7 @@ OutputItem = collections.namedtuple(
     'OutputItem', field_names=['seeds', 'reward', 'steps'])
 
 
-def worker_func(env_name, input_queue, output_queue, nhid,env_seed):
+def worker_func(env_name, input_queue, output_queue, nhid, env_seed, noise_std):
     env = gym.make(env_name)
     cache = {}
 
@@ -86,11 +84,11 @@ def worker_func(env_name, input_queue, output_queue, nhid,env_seed):
             if len(net_seeds) > 1:
                 net = cache.get(net_seeds[:-1])
                 if net is not None:
-                    net = mutate_net(net, net_seeds[-1])
+                    net = mutate_net(net, net_seeds[-1], noise_std)
                 else:
-                    net = build_net(env, net_seeds, nhid)
+                    net = build_net(env, net_seeds, nhid, noise_std)
             else:
-                net = build_net(env, net_seeds, nhid)
+                net = build_net(env, net_seeds, nhid, noise_std)
             new_cache[net_seeds] = net
             reward, steps = evaluate((net,env,env_seed))
             output_queue.put(OutputItem(
@@ -103,6 +101,8 @@ if __name__ == "__main__":
     POPULATION_SIZE = 2000
     PARENTS_COUNT = 10
     WORKERS_COUNT = 6
+    NOISE_STD = 0.01
+
     SEEDS_PER_WORKER = POPULATION_SIZE // WORKERS_COUNT
     MAX_SEED = 2**32 - 1
 
@@ -123,7 +123,7 @@ if __name__ == "__main__":
     for _ in range(WORKERS_COUNT):
         input_queue = mp.Queue(maxsize=1)
         input_queues.append(input_queue)
-        w = mp.Process(target=worker_func, args=(args.env, input_queue, output_queue, args.hid, args.seed))
+        w = mp.Process(target=worker_func, args=(args.env, input_queue, output_queue, args.hid, args.seed, NOISE_STD))
         w.start()
         seeds = [(np.random.randint(MAX_SEED),) for _ in range(SEEDS_PER_WORKER)]
         input_queue.put(seeds)
