cdc921d628d286a9e27b318e0a82b2b78f26accf
simondlevy <simon.d.levy@gmail.com>
2020-07-01 20:57:09 -0400

    More command-line migration

--- Chapter20/05_continuous_ga_batch.py
+++ Chapter20/05_continuous_ga_batch.py
@@ -18,7 +18,6 @@ NOISE_STD = 0.005
 POPULATION_SIZE = 2000
 PARENTS_COUNT = 10
 WORKERS_COUNT = 2
-SEEDS_PER_WORKER = POPULATION_SIZE // WORKERS_COUNT
 
 
 class MultiNoiseLinear(nn.Linear):
@@ -172,6 +171,7 @@ def worker_func(env_name, input_queue, output_queue, nhid, device):
 
 if __name__ == "__main__":
 
+    SEEDS_PER_WORKER = POPULATION_SIZE // WORKERS_COUNT
     MAX_SEED = 2**32 - 1
 
     mp.set_start_method('spawn')
